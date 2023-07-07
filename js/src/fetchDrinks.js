6e4a9f52a803e90716481d58c6f385a7c8ba9e56
stormymayday <stormymayday7@gmail.com>
2023-07-06 19:27:30 -0700

    Fetching the response from the url. Converting response to json and then returning the data

--- js/src/fetchDrinks.js
+++ js/src/fetchDrinks.js
@@ -1,6 +1,17 @@
 const fetchDrinks = async (url) => {
 
-    return 1;
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+
 
 };
 
