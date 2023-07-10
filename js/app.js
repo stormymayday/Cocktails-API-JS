8df9dc1e6e750565c32b4b3a341ed4229bd33f83
stormymayday <stormymayday7@gmail.com>
2023-07-10 07:47:09 -0700

    Created the searchForm.js module and imported in app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import getElement from "./utils/getElement.js";
 import presentDrinks from "./src/presentDrinks.js";
+import './src/searchForm.js';
 
 // Search cocktail by name
 const cocktailsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
