039578bd1fa5dc9531c036739ea474d55230c19e
stormymayday <stormymayday7@gmail.com>
2023-07-10 07:54:37 -0700

    Created a constant to hold base URL. Selecting the '.search-form'

--- js/src/searchForm.js
+++ js/src/searchForm.js
@@ -1,2 +1,7 @@
 import getElement from "../utils/getElement.js";
 import presentDrinks from "./presentDrinks.js";
+
+const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+
+const form = getElement('.search-form');
+console.log(form);
