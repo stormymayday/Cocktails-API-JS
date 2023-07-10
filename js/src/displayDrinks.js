ffa7cc54ff8727b8da967f9776673e51e790bbaa
stormymayday <stormymayday7@gmail.com>
2023-07-09 17:21:53 -0700

    Constructing newDrinks array by mapping over the drinks array

--- js/src/displayDrinks.js
+++ js/src/displayDrinks.js
@@ -19,7 +19,16 @@ const displayDrinks = async ({ drinks }) => {
 
     } else {
 
+        const newDrinks = drinks.map((drink) => {
 
+            return `<a href="./drink.html">
+                <article class="cocktail" data-id="1">
+                    <img src="./assets/cocktail.jpg" alt="martini">
+                        <h3>martini</h3>
+                </article>
+            </a>`;
+
+        }).join('');
 
     }
 
