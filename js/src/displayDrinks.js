2e7c2a2343abe805236bf465bb0c3cde044202bd
stormymayday <stormymayday7@gmail.com>
2023-07-09 17:11:43 -0700

    Selectin the '.title' (error message contailer)

--- js/src/displayDrinks.js
+++ js/src/displayDrinks.js
@@ -3,8 +3,9 @@ import getElement from "../utils/getElement.js";
 const displayDrinks = async ({ drinks }) => {
 
     const section = getElement('.section-center');
+    const errorMessageTitle = getElement('.title');
 
-    return section;
+    return errorMessageTitle;
 
 };
 
