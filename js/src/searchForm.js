2a132dcd324623596605ff5ce102ff4d830b7295
stormymayday <stormymayday7@gmail.com>
2023-07-10 07:59:36 -0700

    Selecting the input field using the 'name' attribute

--- js/src/searchForm.js
+++ js/src/searchForm.js
@@ -4,4 +4,6 @@ import presentDrinks from "./presentDrinks.js";
 const baseURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
 
 const form = getElement('.search-form');
-console.log(form);
+// Selecting the input field using the 'name' attribute
+const input = getElement('[name="drink"]');
+console.log(input);
