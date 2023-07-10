ad7763a6285b9d18db150bb9553be864688caaf3
stormymayday <stormymayday7@gmail.com>
2023-07-09 17:16:02 -0700

    Checking if drinks is null. Displaying the error message and clearing the cocktails container div ('.section-center')

--- js/src/displayDrinks.js
+++ js/src/displayDrinks.js
@@ -5,7 +5,20 @@ const displayDrinks = async ({ drinks }) => {
     const section = getElement('.section-center');
     const errorMessageTitle = getElement('.title');
 
-    return errorMessageTitle;
+    // Checking if drinks is null
+    if (!drinks) {
+
+        // Displaying the error message
+        errorMessageTitle.textContent = 'sorry, no drinks matched your search';
+
+        // Clearing the cocktails container div
+        section.innerHTML = null;
+
+    } else {
+
+
+
+    }
 
 };
 
