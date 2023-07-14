bc599639ece314dd641ec3cc0bdb2de6fac2c005
stormymayday <stormymayday7@gmail.com>
2023-07-13 20:25:27 -0700

    Placing the cocktail ID into the localStorage

--- js/src/setDrink.js
+++ js/src/setDrink.js
@@ -3,11 +3,14 @@ const setDrink = (section) => {
     section.addEventListener('click', (e) => {
 
         // Temporary for debugging
-        e.preventDefault();
+        // e.preventDefault();
 
+        // Getting the cocktail id from the parent '.cocktail' article
         const id = e.target.parentElement.dataset.id;
+        // console.log(id);
 
-        console.log(id);
+        // Placing the cocktail id into the localStorage
+        localStorage.setItem('drink', id);
 
     });
 
