ff3796c5bd758083b3a24cecd1745f0860008cc4
stormymayday <stormymayday7@gmail.com>
2023-07-15 08:26:11 -0700

    Removed the id part from the singleCocktailURL string

--- js/singleDrink.js
+++ js/singleDrink.js
@@ -2,7 +2,7 @@ import fetchDrinks from "./src/fetchDrinks.js";
 import displaySingleDrink from "./src/displaySingleDrink.js";
 
 // Lookup full cocktail details by id
-const singleCocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007';
+const singleCocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
 
 const presentDrink = async () => {
