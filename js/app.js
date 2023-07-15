8f15e216572936c01c6b3c3fc2e6d2babc6b80d6
stormymayday <stormymayday7@gmail.com>
2023-07-15 08:23:55 -0700

    Moved singleCocktailURL from app.js to singleDrink.js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,6 @@ import './src/searchForm.js';
 // Search cocktail by name
 const cocktailsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 
-// Lookup full cocktail details by id
-const singleCocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007';
-
 window.addEventListener("DOMContentLoaded", () => {
 
     presentDrinks(cocktailsURL);
--- js/singleDrink.js
+++ js/singleDrink.js
@@ -1,9 +1,14 @@
 import fetchDrinks from "./src/fetchDrinks.js";
 import displaySingleDrink from "./src/displaySingleDrink.js";
 
+// Lookup full cocktail details by id
+const singleCocktailURL = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007';
+
+
 const presentDrink = async () => {
 
     console.log(`hello from presentDrink`);
+
 
 };
 
