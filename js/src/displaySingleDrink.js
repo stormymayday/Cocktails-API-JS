21a6d524b336f3917da5bb7a64c588da07046c3b
stormymayday <stormymayday7@gmail.com>
2023-07-16 18:05:01 -0700

    Extracting the ingredients and placing them into the array 'list'

--- js/src/displaySingleDrink.js
+++ js/src/displaySingleDrink.js
@@ -6,10 +6,22 @@ const displaySingleDrink = (data) => {
     // Hiding the Loading
     hideLoading();
 
+    // Extracting the first item of the drinks array
     const drink = data.drinks[0];
 
+    // Desctructuring image, name, and description
     const { strDrinkThumb: image, strDrink: name, strInstructions: description } = drink;
 
+    // Extracting the ingredients
+    const list = [
+        drink.strIngredient1,
+        drink.strIngredient2,
+        drink.strIngredient3,
+        drink.strIngredient4,
+        drink.strIngredient5,
+    ];
+
+    console.log(list);
 };
 
 export default displaySingleDrink;
